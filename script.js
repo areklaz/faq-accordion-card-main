2a2869b7a1db2000ed0e91963a6e2d633a3bc7f7
archi1397 <75629951+archi1397@users.noreply.github.com>
2022-03-19 17:15:20 +0100

    solution add

---
+++ script.js
@@ -0,0 +1,34 @@
+const questionBtn = document.querySelectorAll('.question-btn')
+const answer = document.querySelectorAll('.answer')
+const questionBox = document.querySelectorAll('.question-box')
+const arrows = document.querySelectorAll('.arrow')
+
+const showAnswer = (e) => {
+    
+    if (!e.target.parentNode.classList.contains('active')) {
+        questionBox.forEach(questionBox => questionBox.classList.remove('active'))
+        questionBtn.forEach(btn => btn.style.fontWeight = '400')
+        arrows.forEach(arrow => arrow.style.transform = '')
+    }
+    
+    
+    if (e.target.style.fontWeight == 'bold') {
+        e.target.style.fontWeight = ''
+    } else {
+        e.target.style.fontWeight = 'bold'
+    }
+
+    if (e.target.children[0].style.transform == ''){
+        e.target.children[0].style.transform = 'rotate(180deg)'
+    } else {
+        e.target.children[0].style.transform = ''
+    }
+
+    
+    e.target.parentNode.classList.toggle('active')
+    // e.target.parentNode.children[1].classList.toggle('active')
+    console.log(e.target.children[0])
+
+}
+
+questionBtn.forEach(btn => btn.addEventListener('click', showAnswer))
